69177a0eb214f15eafd2d7c043c56d0e53932724
Unstated <55029891+Unstated@users.noreply.github.com>
2019-12-25 16:34:52 -0500

    Changing print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hi new laptop");
+        System.out.println("Nyasha is watching TV");
     }
 }
